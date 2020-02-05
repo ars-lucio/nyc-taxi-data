8842df13a0da65df62c40858ee746bdc38dcf9bc
shiftshuffle27 <60679272+shiftshuffle27@users.noreply.github.com>
2020-02-05 11:02:27 -0600

    spec

--- dataframe_to_db.py
+++ dataframe_to_db.py
@@ -6,30 +6,28 @@ from pandas.io import sql
 from socrata_dataframe import SocrataDataframe
 from database_conn import DataBase
 
-#python dataframe_to_db.py -db ./taxi.db -t taxi2018 -s 2018-01-01 -e 2018-12-31 -l 1000
+# python dataframe_to_db.py -db ./taxi.db -t taxi2018 -s 2018-01-01 -e 2018-12-31 -l 1000
+
 
 @click.command()
 @click.option("--db", "-db", "db_file", required=True,
-    help="Path to db",
-    type=click.Path(exists=True, dir_okay=False, readable=True),
-)
-@click.option("--table","-t","table_name",
-    help="Start Date",
-    )
-@click.option("--start-date","-s","start_date",
-    help="Start Date",
-    )
-
-@click.option("--end-date","-e","end_date",
-    help="End Date",
-    )
-@click.option("--limit","-l","limit",
-    help="limit of rows",
-    )
-
-
-def main(db_file,table_name,start_date,end_date,limit):
-    socrata_dataframe = SocrataDataframe('data.cityofnewyork.us','t29m-gskq',1000)
+              help="Path to db",
+              type=click.Path(exists=True, dir_okay=False, readable=True),
+              )
+@click.option("--table", "-t", "table_name",
+              help="Start Date",
+              )
+@click.option("--start-date", "-s", "start_date",
+              help="Start Date",
+              )
+@click.option("--end-date", "-e", "end_date",
+              help="End Date",
+              )
+@click.option("--limit", "-l", "limit",
+              help="limit of rows",
+              )
+def main(db_file, table_name, start_date, end_date, limit):
+    socrata_dataframe = SocrataDataframe('data.cityofnewyork.us', 't29m-gskq', 1000)
     dt_start_date = datetime.datetime.strptime(start_date, '%Y-%m-%d')
     dt_end_date = datetime.datetime.strptime(end_date, '%Y-%m-%d')
     query_text = f'''select VendorID
@@ -52,7 +50,8 @@ def main(db_file,table_name,start_date,end_date,limit):
     where tpep_pickup_datetime BETWEEN {dt_start_date:'%Y-%m-%dT%H:%M:%S.%f'} AND {dt_end_date:'%Y-%m-%dT%H:%M:%S.%f'}
     limit {limit}'''
     df = socrata_dataframe.get_dataframe_query(query_text)
-    selected = df[['total_amount','passenger_count','trip_distance','tpep_pickup_datetime','tpep_dropoff_datetime','PULocationID','DOLocationID']]
+    selected = df[['total_amount', 'passenger_count', 'trip_distance',
+                   'tpep_pickup_datetime', 'tpep_dropoff_datetime', 'PULocationID', 'DOLocationID']]
 
     db = DataBase(db_file)
     table_taxi = f'''CREATE TABLE IF NOT EXISTS {table_name}
@@ -67,10 +66,9 @@ def main(db_file,table_name,start_date,end_date,limit):
 
     db.run_string(table_taxi)
     print(db.run_string("SELECT name FROM sqlite_master WHERE type='table';"))
-    selected.to_sql(table_name, db.conn,if_exists='append', index = False)
+    selected.to_sql(table_name, db.conn, if_exists='append', index=False)
     table = pd.read_sql_query(f"SELECT * from {table_name}", db.conn)
     print(table)
-
 
 
 if __name__ == "__main__":
