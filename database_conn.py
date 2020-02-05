026f28dab45295eab04d9379da48133e3b588ece
shiftshuffle27 <60679272+shiftshuffle27@users.noreply.github.com>
2020-02-05 10:59:34 -0600

    spec

--- database_conn.py
+++ database_conn.py
@@ -1,30 +1,33 @@
 import sqlite3
+
+
 class DataBase():
-      def __init__(self,db_file):
-            self.db_file = db_file
-            self.conn = self.create_connection()
-      def create_connection(self):
-            self.conn = None
-            try:
-                self.conn = sqlite3.connect(self.db_file)
-                return self.conn
-            except Error as e:
-                print(e)
+    def __init__(self, db_file):
+        self.db_file = db_file
+        self.conn = self.create_connection()
+
+    def create_connection(self):
+        self.conn = None
+        try:
+            self.conn = sqlite3.connect(self.db_file)
             return self.conn
+        except Error as e:
+            print(e)
+        return self.conn
 
-      def run_string(self,sql_string):
-           try:
-               c = self.conn.cursor()
-               c.execute(sql_string)
-               result = c.fetchall()
-               c.close()
-               return result
-           except Error as e:
-               print(e)
+    def run_string(self, sql_string):
+        try:
+            c = self.conn.cursor()
+            c.execute(sql_string)
+            result = c.fetchall()
+            c.close()
+            return result
+        except Error as e:
+            print(e)
 
-      def insert_row(self,table_name,columns,row):
-            sql_string = f''' INSERT INTO {table_name}{tuple([i for i in columns])}
+    def insert_row(self, table_name, columns, row):
+        sql_string = f''' INSERT INTO {table_name}{tuple([i for i in columns])}
                       VALUES{tuple(["?" for i in range(len(columns))])}'''
-            cur = self.conn.cursor()
-            cur.execute(sql_string,row)
-            return cur.lastrowid
+        cur = self.conn.cursor()
+        cur.execute(sql_string, row)
+        return cur.lastrowid
